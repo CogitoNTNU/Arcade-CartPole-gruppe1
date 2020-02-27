e21bf54f5cabe92429fe4bf80e4a50a6c02daa7b
Andreas Gravrok <45593399+joulebit@users.noreply.github.com>
2020-02-27 19:03:06 +0100

    update experimental to decent
    decent prototype now

--- cartpole.py
+++ cartpole.py
@@ -7,11 +7,12 @@ import matplotlib.pyplot as plt
 env = gym.make('CartPole-v1')
 
 # Initialize q-table values to 0
-discretization = 512
-Q = np.zeros((20,discretization, env.action_space.n))    # (states_space, action_space)
+discretization = 10
+pos_discretization = 10
+Q = np.zeros((pos_discretization,discretization, env.action_space.n))    # (states_space, action_space)
 
 # Initialize hyperparameters
-gamma = 0.9         # Discount factor used to balance immediate and future reward.
+gamma = 0.99         # Discount factor used to balance immediate and future reward.
 lr = 0.1            # learning rate
 epsilon = 1         # rate of exploration where 1 is completely random and 0 is deterministic
 
@@ -27,16 +28,19 @@ def running_mean(x, N):
 
 def get_state(cart_position, tip_speed):
     """Returns index of the given tip speed state in the Q-table"""
-    positions = np.linspace(-5,5,num=5)
-    states = np.linspace(-3,3,num=discretization)   # Discretized table of tip speeds. Range of 3 indicates possible speeds
+    positions = np.linspace(-0.3,0.3,num=pos_discretization)
+    states = np.linspace(-4,4,num=discretization)   # Discretized table of tip speeds. Range of 3 indicates possible speeds
     for rounded_cart_position in positions:
         if rounded_cart_position >= cart_position:
-            cart_index = np.where(positions == rounded_cart_position)
+            cart_index = np.where(positions == rounded_cart_position)[0][0]
             for rounded_tip_speed in states:
                 if rounded_tip_speed >= tip_speed:
                     index = np.where(states == rounded_tip_speed)
                     index = index[0][0]
                     return cart_index, index
+    print("NONE",cart_position,cart_index)
+    print(str(index))
+    return None
 
 def better_action(cart_position, tip_speed):
     """Returns index of best action according to Q-table for a given tip speed"""
@@ -50,24 +54,33 @@ def update_q(state, action, new_state, cart_pos, new_cart_pos, reward=1):
 
 
 
-for i_episode in range(6000):
+for i_episode in range(1500):
     observation = env.reset()
     episode_reward = 0
 
+    
     if 4000 < i_episode :
         # Drop exploration
         epsilon = 0
-
+        
+    if i_episode%500 == 0 and i_episode != 0:
+        print(i_episode)
+        print("best run was", max(rewards[-499:]))
     
         
-    for timestep in range(200):
-        if i_episode > 4980:
+    for timestep in range(500):
+        if i_episode > 10000:
             #env.render()
             pass
+
+        if len(rewards) > 1:
+
+            if episode_reward >= max(rewards) and episode_reward > 30:
+                epsilon -= 0.005
     
 
         tip_speed = observation[3]
-        cart_position = observation[0]
+        cart_position = observation[2]
 
         if random.uniform(0, 1) < epsilon:      # Chooses random action often if epsilon is high
             action = env.action_space.sample()
@@ -76,14 +89,14 @@ for i_episode in range(6000):
             action = better_action(cart_position, tip_speed)
 
         # Gather info about changes in environment 
-        state = get_state(cart_position,tip_speed)
+        cart_state, state = get_state(cart_position,tip_speed)
         observation, reward, done, info = env.step(action)
         new_tip_speed = observation[3]
-        new_cart_pos = observation[0]
-        new_state = get_state(new_cart_pos, new_tip_speed)
+        new_cart_pos = observation[2]
+        new_cart_state, new_state = get_state(new_cart_pos, new_tip_speed)
 
         # Update Q-table
-        update_q(state, action, new_state, cart_position, new_cart_pos, reward)
+        update_q(state, action, new_state, cart_state, new_cart_state, reward)
 
         episode_reward += reward
 
